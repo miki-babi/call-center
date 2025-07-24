aed34e79ddeb483e32bb82a11d46ae33d4ff37e0
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-24 19:51:11 +0300

    test

--- resources/views/order/map.blade.php
+++ resources/views/order/map.blade.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Map Search</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+@extends('layout.app')
+
+@section('content')
     <!-- Leaflet CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     
@@ -134,5 +130,4 @@
     }
 </script>
 
-</body>
-</html>
+@endsection
