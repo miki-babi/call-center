933750d5846240de81586fab961297683cbdf9d0
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-24 11:38:47 +0300

    test

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,15 +20,21 @@ setTimeout(() => loading = false, 1000)">
     </div>
 
     <nav class="bg-gray-800 text-white p-4 flex justify-between items-center">
-       
+
         <div role="tablist" class="tabs tabs-border items-center">
-             <div class="text-lg font-semibold">
-            <span>Current branch: </span>
-        </div><div>
-            <a role="tab" class="tab {{ request()->is('shop/orders') ? 'tab-active' : '' }} " href="{{ route('orders.fetchAll') }}">All</a>
-            <a role="tab" class="tab {{ request()->is('shop/orders/mexico') ? 'tab-active' : '' }} " href="{{ route('orders.fetch', ['name' => 'mexico']) }}">Mexico</a>
-            <a role="tab" class="tab {{ request()->is('shop/orders/ayat') ? 'tab-active' : '' }} " href="{{ route('orders.fetch', ['name' => 'ayat']) }}">Ayat</a>
+            <div class="text-lg font-semibold">
+                <span>Current branch: </span>
+            </div>
+            <a role="tab" class="tab {{ request()->is('shop/orders') ? 'tab-active' : '' }} "
+                href="{{ route('orders.fetchAll') }}">All</a>
+            <a role="tab" class="tab {{ request()->is('shop/orders/mexico') ? 'tab-active' : '' }} "
+                href="{{ route('orders.fetch', ['name' => 'mexico']) }}">Mexico</a>
+            <a role="tab" class="tab {{ request()->is('shop/orders/ayat') ? 'tab-active' : '' }} "
+                href="{{ route('orders.fetch', ['name' => 'ayat']) }}">Ayat</a>
             <a role="tab" class="tab tab-disabled">Kadisco</a>
+        </div>
+        <div class="p-4 bg-blue-500 text-white ">
+            create new order 
         </div>
     </nav>
 
