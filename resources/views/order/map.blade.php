5ab6f92a1d1ece458755b8927964305445ad6dd1
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-24 16:50:27 +0300

    test

--- resources/views/order/map.blade.php
+++ resources/views/order/map.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Leaflet CSS -->
-    {{-- <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" /> --}}
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     
     <style>
         #map {
