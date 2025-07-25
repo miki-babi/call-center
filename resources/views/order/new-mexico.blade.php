259c3ee1008b1e1a90c5cf0721764dc537178db4
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-25 17:30:44 +0300

    test

--- resources/views/order/new-mexico.blade.php
+++ resources/views/order/new-mexico.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
     <!-- Leaflet CSS -->
-   
 
-
-        <x-product :products="$allProducts"/>
-
-        <x-map shop="mexico" lat="9.03" lon="38.74"/>
-     <x-fields/>
-
-    @endsection
+    <x-product :products="$allProducts" />
+    <x-map shop="mexico" lat="9.03" lon="38.74" />
+    <x-fields />
+    
+@endsection
