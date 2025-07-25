f9772e323dfa9c51bf3ef7ef65a280c6fc6e152e
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-25 16:03:17 +0300

    test

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -34,7 +34,10 @@ setTimeout(() => loading = false, 1000)">
             <a role="tab" class="tab tab-disabled">Kadisco</a>
         </div>
         <div class="p-4 bg-blue-500 text-white rounded-sm ">
+            <a href="{{ route('orders.new') }}">
             create new order 
+
+            </a>
         </div>
     </nav>
 
