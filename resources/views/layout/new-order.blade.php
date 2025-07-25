5ff6d1fb5f9798aaa6fd99f0de9d1538475a32e6
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-25 17:05:50 +0300

    test

--- resources/views/layout/new-order.blade.php
+++ resources/views/layout/new-order.blade.php
@@ -6,6 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Farmer Dashboard')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+
+    <style>
+        #map {
+            margin-top: 20px;
+            /* space between search/results and map */
+            height: 500px;
+            width: 100%;
+            /* z-index: -10; */
+        }
+
+        .search-box {
+            margin: 10px;
+            padding-bottom: 10px;
+            margin-top: 10px;
+        }
+
+        .search-results {
+            margin-top: 35px;
+            background: #fff;
+            border: 1px solid #ccc;
+            max-height: 150px;
+            overflow-y: auto;
+            color: #000;
+            font-weight: bold;
+            position: absolute;
+            z-index: 1000;
+            width: 300px;
+        }
+
+        .search-results div {
+            margin-top: 5px;
+            padding: 5px;
+            cursor: pointer;
+        }
+
+        .search-results div:hover {
+            background: #eee;
+        }
+
+        .distance-display {
+            margin: 10px;
+            font-weight: bold;
+        }
+
+        .product-item,
+        .mb-8 {
+            display: none;
+        }
+    </style>
 </head>
 
 <body x-data="{ loading: false }" x-init="loading = true;
--- resources/views/order/new-mexico.blade.php
+++ resources/views/order/new-mexico.blade.php
@@ -2,59 +2,9 @@
 
 @section('content')
     <!-- Leaflet CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+   
 
-    <style>
-        #map {
-            margin-top: 20px;
-            /* space between search/results and map */
-            height: 500px;
-            width: 100%;
-            /* z-index: -10; */
-        }
 
-        .search-box {
-            margin: 10px;
-            padding-bottom: 10px;
-            margin-top: 10px;
-        }
-
-        .search-results {
-            margin-top: 35px;
-            background: #fff;
-            border: 1px solid #ccc;
-            max-height: 150px;
-            overflow-y: auto;
-            color: #000;
-            font-weight: bold;
-            position: absolute;
-            z-index: 1000;
-            width: 300px;
-        }
-
-        .search-results div {
-            margin-top: 5px;
-            padding: 5px;
-            cursor: pointer;
-        }
-
-        .search-results div:hover {
-            background: #eee;
-        }
-
-        .distance-display {
-            margin: 10px;
-            font-weight: bold;
-        }
-
-        .product-item,
-        .mb-8 {
-            display: none;
-        }
-    </style>
-    </head>
-
-    <body>
         <x-product :products="$allProducts"/>
 
         <x-map/>
