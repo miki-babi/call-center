2ea9456dd165e98ec243f8703ea65a9458777402
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-25 09:20:29 +0300

    test

--- resources/views/order/newOrder.blade.php
+++ resources/views/order/newOrder.blade.php
@@ -5,9 +5,9 @@
 <div>
     <div>
         <a role="tab" class="tab {{ request()->is('shop/orders/mexico') ? 'tab-active' : '' }} "
-                href="{{ route('orders.fetch', ['name' => 'mexico']) }}">Mexico</a>
+                href="{{ route('orders.new', ['branch' => 'mexico']) }}">Mexico</a>
             <a role="tab" class="tab {{ request()->is('shop/orders/ayat') ? 'tab-active' : '' }} "
-                href="{{ route('orders.fetch', ['name' => 'ayat']) }}">Ayat</a>
+                href="{{ route('orders.new', ['branch' => 'ayat']) }}">Ayat</a>
     </div>
 </div>
 
