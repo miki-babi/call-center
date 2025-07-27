e880814fe52c266d44dd58df5ac22c55ad832c1c
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-27 21:10:28 +0300

    test

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -1,5 +1,5 @@
 @props(['shop','lat','lon','deliveryOptions'])
-
+<div>✅ MAP COMPONENT LOADED</div>
 <div class="search-box w-full justify-end flex pr-4 mt-8">
             <input type="text" id="search" placeholder="Search location..." style="width: 300px; padding: 6px;">
             <div id="results" class="search-results"></div>
