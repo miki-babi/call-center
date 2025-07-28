d562dadf39d5e4f19c6cce4f3dc360c242dc1231
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-28 14:44:01 +0300

    test

--- resources/views/order/new-mexico.blade.php
+++ resources/views/order/new-mexico.blade.php
@@ -4,7 +4,7 @@
 
     <x-product :products="$allProducts" :delivery-options="$deliveryOptions"/>
     <x-map shop="mexico" lat="9.009907758314755" lon="38.75883353263652" :delivery-options="$deliveryOptions" />
-    <x-fields :delivery-options="$deliveryOptions"/>
+    <x-fields :delivery-options="$deliveryOptions" branch="mexico"/>
 
 
 
