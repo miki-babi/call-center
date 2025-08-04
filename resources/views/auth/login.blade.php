a2a1f10a82571f5eaee3b348091d9492b619813b
miki-babi <mikiyasshiferaw@gmail.com>
2025-08-04 16:00:35 +0300

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Agent Login')</title>
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"> --}}
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     @vite(["resources/css/app.css", "resources/js/app.js"])
 </head>
