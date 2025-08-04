c614cce40032e713d8562db441ae7e45848ec730
miki-babi <mikiyasshiferaw@gmail.com>
2025-08-04 10:16:41 +0300

    test

--- resources/views/layout/new-order.blade.php
+++ resources/views/layout/new-order.blade.php
@@ -103,7 +103,7 @@ setTimeout(() => loading = false, 1000)">
             <a role="tab" class="tab tab-disabled">Kadisco</a>
         </div>
         <div class="p-4 bg-blue-500 text-white rounded-sm ">
-            <a" href="{{ route('orders.index') }}">Orders</a>
+            <a href="{{ route('orders.index') }}">Orders</a>
         </div>
         
     </nav>
