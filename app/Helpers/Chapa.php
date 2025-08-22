efb8d7cd18efcf765fea4f8025c358e6ead58b49
miki-babi <mikiyasshiferaw@gmail.com>
2025-08-23 00:21:37 +0300

    test

--- app/Helpers/Chapa.php
+++ app/Helpers/Chapa.php
@@ -5,12 +5,16 @@ namespace App\Helpers;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
+use Illuminate\Support\Str;
+
 class Chapa
 {
  
     public static function initiate()
     {
+
         // dd('test');
+        $tx_ref = Str::random(10) . '-' . time();
         $response = Http::withHeaders([
             'Authorization' => "Bearer " . env('Chapa_Secretkey'),
             'Content-Type' => 'application/json',
@@ -21,7 +25,7 @@ class Chapa
             'first_name' => 'Bilen',
             'last_name' => 'Gizachew',
             'phone_number' => '0912345678',
-            'tx_ref' => 'chewatatest-6609',
+            'tx_ref' => $tx_ref,
             'callback_url' => 'https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60',
             'return_url' => 'https://www.google.com/',
             'customization' => [
