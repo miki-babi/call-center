de2c23fbf1523c15617a2d6a9c2a9ae7be0389a6
miki-babi <mikiyasshiferaw@gmail.com>
2025-08-22 23:48:43 +0300

    test

--- app/Helpers/Chapa.php
+++ app/Helpers/Chapa.php
@@ -21,7 +21,7 @@ class Chapa
             'first_name' => 'Bilen',
             'last_name' => 'Gizachew',
             'phone_number' => '0912345678',
-            'tx_ref' => 'chewatatest-6669',
+            'tx_ref' => 'chewatatest-6609',
             'callback_url' => 'https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60',
             'return_url' => 'https://www.google.com/',
             'customization' => [
